6d0863c4a13b0d85ff1a656312b1edb51c808466
shillxzy <bzae03014@example.com>
2025-06-08 14:01:55 +0300

    add: JWT - Program.cs

--- KnowledgeCheck.JWT/Program.cs
+++ KnowledgeCheck.JWT/Program.cs
@@ -1,36 +1,82 @@
+using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using KnowledgeCheck.BLL;
+using KnowledgeCheck.BLL.Configuration;
+using KnowledgeCheck.DAL;
+using KnowledgeCheck.JWT.Configuration;
+using KnowledgeCheck.JWT.Middlewares;
 
-namespace KnowledgeCheck.JWT
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// SMTP config
+var smtpSettings = configuration.GetSection("Smtp");
+var smtpClient = new SmtpClient(smtpSettings["Host"])
 {
-    public class Program
+    Port = int.Parse(smtpSettings["Port"] ?? "587"),
+    Credentials = new NetworkCredential(smtpSettings["User"], smtpSettings["Pass"]),
+    EnableSsl = true
+};
+
+builder.Services
+    .AddFluentEmail(smtpSettings["Sender"])
+    .AddSmtpSender(smtpClient);
+
+// Custom services
+builder.Services.AddDALServices(configuration);
+builder.Services.AddBusinessLogic();
+builder.Services.AddMapsterConfiguration();
+
+// JWT
+builder.Services.AddJwtAuthentication(configuration);
+builder.Services.AddAuthorization();
+
+// Controllers
+builder.Services.AddControllers();
+
+// Swagger + JWT
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    var jwtSecurityScheme = new OpenApiSecurityScheme
     {
-        public static void Main(string[] args)
+        BearerFormat = "JWT",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Enter your JWT Access Token",
+        Reference = new OpenApiReference
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
         }
-    }
+    };
+    options.AddSecurityDefinition("Bearer", jwtSecurityScheme);
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, [] }
+    });
+});
+
+var app = builder.Build();
+
+// Dev-specific middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
